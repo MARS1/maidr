a91852a0f212dec6b7ad96aa767148d414641ba3
Marcelo Berta <marcelo.berta@gmail.com>
2014-01-12 12:21:43 -0500

    First Commit - Added all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Maidr::Application.config.secret_key_base = '559ffd9832ae9907798e3d2b6fe1c1b86fbb22dda6d1c945b0bc0b0ce31b0d4b460ab443a979aabe0f424029ffc1b4321a17196732f4ce90f3c287d8116c1930'
